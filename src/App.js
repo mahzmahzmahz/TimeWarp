46fb1de55e07fe8718764be9dfaf04cb30d59ee1
Alex Marz <alexhmarz@gmail.com>
2021-04-22 19:47:25 -0400

    datepicker added and restricted to 1970-until 13 years ago

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import styled from "styled-components"
 import './App.css';
-import { useState, useCallback, Component } from "react"
+import { useState } from "react"
 import Header from "./Header"
 import Video from "./Video"
+import DatePicker from 'react-date-picker'
+
 
 
 
 function App() {
   const [video, setVideo] = useState(false)
+  const [date, setDate] = useState(new Date())
   
+  //This makes it impossible to set your birthday any time after 13 years ago.
+  const hoop = new Date(new Date().setDate(new Date().getDate()-4748))
 
-  
+
+  function handleChangeDate(value){
+    setDate(value)
+    
+  }
+  console.log(date)
 
   return (
     <div>
+
       <Header/>
-      
-      {video ? <Video/> : null}
+        <Container>
+
+          <DatePicker
+            onChange={handleChangeDate}
+            value={date}
+            minDate={new Date("01-01-1970")}
+            maxDate={new Date(hoop)}
+          />
+
+          {video ? <Video/> : null}
+
+      </Container>
+
     </div>
   );
 }
 
 
 export default App;
+
+const Container = styled.section`
+    padding:10%;
+    display: flex;
+    flex-wrap: flex;
+    justify-content: center;
+  `
